c57fcc29e5b19002dda8abc4de3dac08df1c93e2
arjunb-infosys <53328788+arjunb-infosys@users.noreply.github.com>
2019-07-30 11:16:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ var express 		    = require('express');
 var app			        = express();	
 var request			    = require('request');
 var bodyParser          = require('body-parser');
+const appConfig = require('config.json')
 var myPort= process.env.PORT || 3000;
 app.post('/authenticateUser',function(req,res){
+ const userCreds = appConfig.loginCreds;
+try{
+ if(userCreds[req.body.username]){
+
+}else{
+
+}
+ }catch(e){
  
+ }
 })
 app.listen(myPort);
