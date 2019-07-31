afc0f7b9f833315fcfc9590732aab731b1139582
arjunb-infosys <53328788+arjunb-infosys@users.noreply.github.com>
2019-07-31 09:34:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
-
 var express = require('express');
-var app = express.Router();
-var request = require('request');
 var bodyParser = require('body-parser');
-var appConfig = require('config.json');
-var myPort= process.env.PORT || 3000;
+var routes = require('./routes');
+var redirect = require("express-redirect");
+var app = express();
+var session = require('express-session');
+var port = process.env.PORT || 3000;
+app.use(session({ secret: 'this-is-a-secret-token',resave: true, saveUninitialized: true, cookie: { maxAge: 60000 }}));
+redirect(app);
+//global.recentInput = "";
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect    
+	res.setHeader('Access-Control-Allow-Origin','*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false })) 
 app.use(bodyParser.json());
-app.post('/authenticateUser',function(req,res){
- const userCreds = appConfig.loginCreds;
-try{
- if(userCreds[req.body.username]){
-
-}else{
-
-}
- }catch(e){
- 
- }
+app.set('views', __dirname + '/public/views');
+app.set('view engine', 'ejs');
+app.use(routes);
+global.bookingInfo = {
+	tickets:{},
+	seatsInfo:{}
+};
+var server = app.listen(port,function(){
+	console.log("Application started listening port "+port);		
 });
-
-app.get('/testing1',function(req,res){
-    res.send("This is a test value");
-});
-
-app.post('/testing',function(req,res){
-    res.send("This is a test value");
-});
-
-
-app.listen(myPort);
