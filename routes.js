afa65e95db1b005cd91070824dae4c777e43f727
arjunb-infosys <53328788+arjunb-infosys@users.noreply.github.com>
2019-07-31 09:53:15 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ var request			= require('request');
 var fs 				= require("fs");	
 var request			= require('request');
 var path			= require("path");	
-var config			= require('./config');	
+var config			= require('./config.json');	
 const uuidv1 		= require('uuid/v1');
 
 //var Authentication = require('./utilities/Authentication');
@@ -19,4 +19,35 @@ router.get('/testing',function(req,res){
 res.send("Success");
 });
 
+router.post('/authenticateUser',function(req,res){
+  try{
+    const credConfig = config['loginCreds'];
+if(credConfig[req.body.username]){
+if(credConfig[req.body.username].password == req.body.password){
+res.json({
+"success":true
+  ,"msg":credConfig[req.body.username].fullName
+}).end();
+}else{
+
+    res.json({
+        "success":false
+          ,"msg":"Invalid credentials"
+        })
+}
+}else{
+    res.json({
+        "success":false
+          ,"msg":"Username does not exist"
+        }).end();
+
+}
+
+    }catch(e){
+        res.json({
+            "success":false
+              ,"msg":"Oops!! There seems to some problem in the back end. We regret the inconvenience."
+            }).end();
+    }
+});
 module.exports = router;
