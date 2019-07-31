567be1ba8fec60ae2bfb2d4f0de0e9bc38454fe4
arjunb-infosys <53328788+arjunb-infosys@users.noreply.github.com>
2019-07-31 09:12:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var request = require('request');
 var bodyParser = require('body-parser');
 var appConfig = require('config.json');
 var myPort= process.env.PORT || 3000;
-
+app.use(bodyParser.urlencoded({ extended: false })) 
+app.use(bodyParser.json());
 app.post('/authenticateUser',function(req,res){
  const userCreds = appConfig.loginCreds;
 try{
@@ -19,7 +20,11 @@ try{
  }
 });
 
-app.get('/test',function(req,res){
+app.get('/testing1',function(req,res){
+    res.send("This is a test value");
+});
+
+app.post('/testing',function(req,res){
     res.send("This is a test value");
 });
 
